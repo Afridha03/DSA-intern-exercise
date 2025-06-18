d331a80aca6ae451f9820f74ba526b5c708c764d
Afridha03 <afridhashaheen@gmail.com>
2025-06-19 00:05:07 +0530

    version 1

---
+++ ex7.py
@@ -0,0 +1,11 @@
+matrix_2d = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+rotated_matrix = []
+
+for i in range(len(matrix_2d)):
+    row = []
+    for j in range(len(matrix_2d) - 1, -1, -1):
+        row.append(matrix_2d[j][i])
+    rotated_matrix.append(row)
+
+for row in rotated_matrix:
+    print(row)
