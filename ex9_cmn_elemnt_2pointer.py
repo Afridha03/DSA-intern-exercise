8a29a62e32b1890a68e2632f8943020fd0a27265
Afridha03 <afridhashaheen@gmail.com>
2025-06-23 00:06:29 +0530

    variable changed

--- ex9_cmn_elemnt_2pointer.py
+++ ex9_cmn_elemnt_2pointer.py
@@ -1,16 +1,16 @@
 #input for the first array
-n1 = int(input("Enter number of elements in first array: "))
+num1 = int(input("Enter number of elements in first array: "))
 arr1 = []
 print("Enter elements for first array:")
-for index in range(n1):
+for index in range(num1):
     num = int(input())
     arr1.append(num)
 
 # input for the second array
-n2 = int(input("Enter number of elements in second array: "))
+num2 = int(input("Enter number of elements in second array: "))
 arr2 = []
 print("Enter elements for second array:")
-for index in range(n2):
+for index in range(num2):
     num = int(input())
     arr2.append(num)
 
@@ -18,20 +18,20 @@ for index in range(n2):
 arr1.sort()
 arr2.sort()
 
-i = len(arr1) - 1
-j = len(arr2) - 1
+pointer1 = len(arr1) - 1
+pointer2 = len(arr2) - 1
 found = False
 
 
-while i >= 0 and j >= 0:
-    if arr1[i] == arr2[j]:
-        print("Largest common element is:", arr1[i])
+while pointer1 >= 0 and pointer2 >= 0:
+    if arr1[pointer1] == arr2[pointer2]:
+        print("Largest common element is:", arr1[pointer1])
         found = True
         break
-    elif arr1[i] > arr2[j]:
-        i = i - 1
+    elif arr1[pointer1] > arr2[pointer2]:
+        pointer1 = pointer1 - 1
     else:
-        j = j - 1
+        pointer2 = pointer2 - 1
 
 if found == False:
     print("No common element found.")
